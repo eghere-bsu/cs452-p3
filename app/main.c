02984cdf9bf77e9eb9200ad1632db7bc72059cbb
eghere-bsu <112023370+eghere-bsu@users.noreply.github.com>
2024-09-23 21:13:00 +0000

    Create print version command

--- app/main.c
+++ app/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <unistd.h> 
+#include "../src/lab.h" 
 
-int main(void)
+void print_version()
 {
+  printf("Shell version: %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+}
+
+int main(int argc, char *argv[])
+{
+  int opt;
+
+  // Parse command-line arguments
+  while ((opt = getopt(argc, argv, "v")) != -1)
+  {
+    switch (opt)
+    {
+    case 'v':
+      print_version();
+      return 0;
+    default:
+      printf("Usage: %s [-v]\n", argv[0]);
+      return 1;
+    }
+  }
+
   printf("hello world\n");
   return 0;
 }
